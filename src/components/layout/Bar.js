fbaeda38c760506a6785758fcf2fcf498dbd695b
crusiris <cruzsirys@gmail.com>
2020-05-20 13:27:17 -0400

    Componente de tareas y formulario para agregar tareas

---
+++ src/components/layout/Bar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Bar = () => {
+    return ( 
+        <header className="app-header">
+            <p className="nombre-usuario">Hola <span>Crusiris Nuñez</span> </p>
+            <nav className="nav-principal">
+                <a href="#!">Cerrar sesion</a>
+            </nav>
+
+        </header>
+     );
+}
+ 
+export default Bar;
--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import Sidebar  from '../layout/Sidebar';
- 
+import Bar  from '../layout/Bar';
+import FormTask  from '../task/FormTask';
+
 const Projects = () => {
     return ( 
         <div className="contenedor-app">
             <Sidebar/>
 
             <div className="seccion-principal">
+                <Bar/>
                 <main>
-
+                    <FormTask/>
                     <div className="contenedor-tareas">
 
                     </div>
