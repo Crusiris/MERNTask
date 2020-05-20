6fd810a37d313a6a6f7f1d70c0e6ec621af4ff46
crusiris <cruzsirys@gmail.com>
2020-05-19 22:26:26 -0400

    State y funciones para el Componente Login

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,8 +1,79 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { Link } from 'react-router-dom';
  
 const Login = () => {
+
+    //State para datos login
+    const [ user, saveUser ] = useState({
+        email:'',
+        password:''
+    })
+
+    //Destructuring de user
+    const { email, password } =  user;
+
+    //Obteniendo los datos del input y guardandolos en el state
+    const onChangeLogin= (e) =>{
+        saveUser({
+            ...user,
+            [e.target.name] : e.target.value
+        })
+    }
+
+    const onSubmit = e =>{
+        e.preventDefaul();
+
+        //Validaciones
+
+        //Funcion que inicia sesion
+    }
+
     return ( 
-        <h1>Desde Login</h1>
+        <div className="form-usuario">
+            <div className="contenedor-form sombra-dark" >
+                    <h1>Iniciar sesion</h1>
+
+                    <form
+                    onSubmit={onSubmit}
+                    >
+                        <div className="campo-form">
+                            <label htmlFor="email">Email</label>
+                            <input 
+                                type="email"
+                                id="email"
+                                name="email"
+                                value={email}
+                                placeholder="Tu Email"
+                                onChange={onChangeLogin}
+                                />
+                        </div>
+
+                        <div className="campo-form">
+                            <label htmlFor="Password">Password</label>
+                            <input 
+                                type="password"
+                                id="password"
+                                name="password"
+                                value={password}
+                                placeholder="Tu Password"
+                                onChange={onChangeLogin}
+                                />
+                        </div>
+
+                        <div className="campo-form">
+                            <input
+                            type="submit" className="btn btn-primario btn-block"
+                            value="Iniciar Sesion"
+                            />
+                        </div>
+                    </form>
+
+                    <Link to={'/nueva-cuenta'} className="enlace-cuenta">
+                        Obtener Cuenta
+                    </Link>
+            </div>
+
+        </div>
      );
 }
  
